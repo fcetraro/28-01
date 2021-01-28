6454f672746d64b6bac4b34f161123b6b19de281
Federico Cetraro <>
2021-01-28 13:51:32 -0300

    Add test of Calculator

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.handler.AgeHandler;
 import com.example.demo.handler.CalculatorHandler;
 import com.example.demo.model.HouseDTO;
 import org.springframework.web.bind.annotation.*;
